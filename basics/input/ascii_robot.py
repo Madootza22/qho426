62b0329d26584da52fafca791edd3c97a3ba73f2
Madalina93 <>
2021-11-04 21:51:32 +0000

    Added code to display an ASCII robot with dynamic eyes.

--- basics/input/ascii_robot.py
+++ basics/input/ascii_robot.py
@@ -1,9 +1,16 @@
-print("   () ()")
-print("    | |")
-print("   -----")
-print("@--|    |--@")
-print("   |    |")
-print("   ------")
-print("    | |")
-print("    | |")
-print("  @_   _@")
+print(""" 
+##########
+#  o  o  #
+#  ----  #
+##########
+""")
+
+# Ask user for eye character 
+print("Please enter eye character")
+eye = input() 
+
+# Display an ascii art robot
+print("##########")
+print(f"#  {eye}  {eye}  #")
+print("#  ----  #")
+print("##########")
