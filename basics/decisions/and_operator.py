57a302eaeaa9b24d36c4b018e3352ce7e27ac75c
Madalina93 <>
2021-11-09 14:30:56 +0000

    Added code to demonstra logical and operator.

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,11 @@
+#Retreive what to see and hear from the user
+print("\nWhat did I hear?")
+hear = input()
+print("\nWhat did I see?")
+see = input()
+
+#Identify and display the answer
+if ((hear == "grrr") and (see == "two red eyes")):
+  print("\nThere is a scary creature. I should get out of here! ")  
+else:
+  print("\nI am a little scared but I will continue!")  
