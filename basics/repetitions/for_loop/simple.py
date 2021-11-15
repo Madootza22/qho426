cc49f076caf336e57bc356b4f2a964d6a83704a9
Madalina93 <>
2021-11-15 05:45:53 +0000

    Added code to demonstrate the use of a simple for loop.

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,18 @@
+#Asking the user to introduce a number
+print("\nHow many mountains should I display? ")
+mountains = int(input())
+
+#Display appropriate message
+print("\nDisplaying...")
+
+#Creating an ascii using for loop
+for mountains in range (mountains):
+  print("      --")
+  print("    /     \_")
+  print("   /^      \ ")
+  print("  /  ^      \_")
+  print("_/ ^ ^       ^\ ")
+  print("/   ^      ^   \ ")
+
+print("\nDone!")
+
