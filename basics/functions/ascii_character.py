e6da07958da0e119fff55e29471ac32822a50a22
Madalina93 <>
2021-11-15 19:34:37 +0000

    Added code to demonstrate tthe use of len, ord functions.rate the use of len, ord, chr and a user-defined function.

---
+++ basics/functions/ascii_character.py
@@ -0,0 +1,12 @@
+#display message
+print("Program Started!")
+#Ask user to introduce an ASCII code
+print("Please enter an ASCII code:")
+code = int(input())
+
+if (code >= 32 and code <= 126):
+  print("The character represented by the ASCII code is ".format(code), chr(code))
+else:
+  print("The character cannot be displayed.")
+
+print("Program Ended!")
