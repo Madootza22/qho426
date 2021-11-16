ba53f067508cac7b16c1062d3da35f57bf8fe9a9
Madalina93 <93394711+Madootza22@users.noreply.github.com>
2021-11-16 09:14:09 +0000

    Added code to demonstrate the use of integrated functions.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,13 @@
+#Display message
+print("\nProgram started!")
+
+#Ask user to enter character
+print("Please enter a standard character: ")
+character = input()
+
+if len(character) == 1:
+  print("The ASCII code for {} is {}".format(character, ord(character)))
+else:
+  print(("You should introduce just one character!"))
+print()
+print("\nProgram ended!")
---
+++ basics/functions/simple_functions.py
@@ -0,0 +1,7 @@
+#Defining the function
+def listen():
+  print("\nWhat king of sound do I hear? ")
+  sound = input()
+  print("\nThat was a loud", sound, "!")
+
+listen()  
